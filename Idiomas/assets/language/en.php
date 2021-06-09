c74576bcf119de5663902bfd6e0bbad1e63e84b5
Marcelo Martins <81728683+Mr-MarceloDev@users.noreply.github.com>
2021-06-09 00:25:19 -0300

    Add files via upload

---
+++ Idiomas/assets/language/en.php
@@ -0,0 +1,17 @@
+
+<?php
+$lang = array(
+	"titulo" => "WEBSITE WITH TWO LANGUAGES",
+	"home" => "Home",
+	"sobre" => "About",
+	"contato" => "Contact",
+	"idioma" => "Language",
+	"en" => "English",
+	"pt" => "Portuguese",
+	"h1" => "This is an example page in two languages",
+	"descricao" =>
+	"Computer science is the science that studies the techniques, methodologies, computational instruments and technological applications, which computerize processes and develop solutions for processing input and output data based on the computer. It is not restricted to the study of algorithms, their applications and implementation in the form of software. Thus, Computer Science also encompasses data modeling techniques and database management, also involving telecommunication and communication protocols, in addition to principles that encompass other specializations in the area. , is classified as exact science, despite inheriting elements of Aristotelian philosophical logic, having therefore an important role in the mathematical formalization of algorithms, as a way of representing decidable problems, ie, those that are susceptible to reduction to basic, capable elementary operations. to be reproduced through any device capable of storing r and manipulate data. One of these devices is the digital computer, which is in widespread use today. Also of fundamental importance for the field of Computer Science are the methodologies and techniques linked to software implementation that address the specification, modeling, coding, testing and evaluation of software systems.
+	 Studies from Computer Science can be applied in any area of ​​human knowledge where it is possible to define problem solving methods based on previously observed repetitions. Recent advances in Computer Science have strongly impacted contemporary society, in particular applications related to the areas of computer networking, Internet, Web, data science and mobile computing, which have been used by billions of people around the globe.
+	",
+);
+?>
